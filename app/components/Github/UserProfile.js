7bd2a9e8be85c720ecf05d05acba5eecd613efc8
elpenao <elpenao@gmail.com>
2016-02-14 13:57:37 -0500

    github integrated

---
+++ app/components/Github/UserProfile.js
@@ -0,0 +1,19 @@
+var React = require('react');
+
+var UserProfile = React.createClass({
+	propTypes: {
+		username: React.PropTypes.string.isRequired,
+		bio: React.PropTypes.object.isRequired
+	},
+	render: function(){
+		console.log(this.props.bio);
+		return (
+			<div>
+				<p>User Profile</p>
+				<p>Username: {this.props.username}</p>
+			</div>
+		)
+	}
+})
+
+module.exports = UserProfile;
