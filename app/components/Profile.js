b61f7cd9d1f0b172534e6811b44406e6c47a3068
elpenao <elpenao@gmail.com>
2016-02-17 15:57:51 -0500

    auth test

--- app/components/Profile.js
+++ app/components/Profile.js
@@ -18,6 +18,16 @@ var Profile = React.createClass({
   },
   componentDidMount: function(){
     this.ref = new Firebase('https://sweltering-heat-7072.firebaseio.com/');
+        // Create a callback which logs the current auth state
+    function authDataCallback(authData) {
+      if (authData) {
+        console.log("User " + authData.uid + " is logged in with " + authData.provider);
+      } else {
+        console.log("User is logged out");
+      }
+    }
+    // Register the callback to be fired every time auth state changes
+    this.ref.onAuth(authDataCallback);
     this.init(this.props.params.username)
   },
   componentWillReceiveProps: function(nextProps){
